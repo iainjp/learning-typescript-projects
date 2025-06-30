abfb4467e7511ccdbff3ff57602eb378f9416b99
Iain Pritchard <iainpritchard92@gmail.com>
2025-06-30 19:18:25 +0100

    01 01 rewrite in modern JS

--- projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/01-syntactic-sugar/index.js
@@ -1,4 +1,21 @@
 // Put your announceMachines function here! ✨
-// See ./original.js for its older JavaScript code.
+
+const announceMachines = (announce, ...machines) => {
+	let labelCount = 0;
+
+	const getLabel = (machine) => {
+		if (machine.label) {
+			labelCount += 1;
+			return machine.label;
+		}
+		return `Make: ${machine.make}; Model: ${machine.model}`;
+	};
+
+	for (const machine of machines) {
+		announce(getLabel(machine));
+	}
+
+	return labelCount;
+};
 
 module.exports.announceMachines = announceMachines;
